2ded36ed922d123f3ac3289c5da01174a961520e
Jordy <vijfeijken.jordy@gmail.com>
2017-01-13 14:51:54 +0100

    toetsplanningtoevoegen en toetsplanningbeheren toegevoegd

---
+++ OWL-LEARN/ToetsPlanningBeheren.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OWL_LEARN
+{
+    /// <summary>
+    /// Interaction logic for ToetsPlanningBeheren.xaml
+    /// </summary>
+    public partial class ToetsPlanningBeheren : Window
+    {
+        struct LvLeerlingInfo
+        {
+            public string LeerlingVoornaam { get; set; }
+            public string LeerlingAchternaam { get; set; }
+            public string LeerlingId { get; set; }
+        }
+        public ToetsPlanningBeheren()
+        {
+            InitializeComponent();
+            PopulateLvLeerlingen();
+        }
+        private void PopulateLvLeerlingen()
+        {
+            DataTable dtLeerlingen = new DBS().Search("users", "RolID", "2");
+            List<LvLeerlingInfo> lstLeerlingen = new List<LvLeerlingInfo>();
+
+            foreach (DataRow drLeerlingen in dtLeerlingen.Rows)
+            {
+                lstLeerlingen.Add(new LvLeerlingInfo() { LeerlingId = drLeerlingen[0].ToString(), LeerlingVoornaam = drLeerlingen[3].ToString(), LeerlingAchternaam = drLeerlingen[4].ToString() });
+            }
+            lvLeerlingen.ItemsSource = lstLeerlingen;
+        }
+
+        private void btAddToetsPlanning_Click(object sender, RoutedEventArgs e)
+        {
+            ToetsPlanningToevoegen TPT = new ToetsPlanningToevoegen();
+            TPT.Show();
+            this.Close();
+        }
+    }
+}
---
+++ OWL-LEARN/ToetsPlanningToevoegen.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OWL_LEARN
+{
+    /// <summary>
+    /// Interaction logic for ToetsPlanningToevoegen.xaml
+    /// </summary>
+    public partial class ToetsPlanningToevoegen : Window
+    {
+        string sGekozenLeerlingUsername;
+        string sGekozenVakId;
+        struct Vakken
+        {
+            public string VakNaam { get; set; }
+            public string VakId { get; set; }
+        }
+        struct LvLeerlingInfo
+        {
+            public string LeerlingVoornaam { get; set; }
+            public string LeerlingAchternaam { get; set; }
+            public string LeerlingUsername { get; set; }
+        }
+        struct Lesonderdelen
+        {
+            public string Lesonderdeel { get; set; }
+            public string LesonderdeelId { get; set; }
+        }
+
+        public ToetsPlanningToevoegen()
+        {
+            InitializeComponent();
+            PopulateLvLeerlingen();
+            FillCbKiesVak();
+        }
+
+        private void PopulateLvLeerlingen()
+        {
+            DataTable dtLeerlingen = new DBS().Search("users", "RolID", "2");
+            List<LvLeerlingInfo> lstLeerlingen = new List<LvLeerlingInfo>();
+
+            foreach (DataRow drLeerlingen in dtLeerlingen.Rows)
+            {
+                lstLeerlingen.Add(new LvLeerlingInfo() { LeerlingUsername = drLeerlingen[1].ToString(), LeerlingVoornaam = drLeerlingen[3].ToString(), LeerlingAchternaam = drLeerlingen[4].ToString() });
+            }
+            lvLeerlingen.ItemsSource = lstLeerlingen;
+        }
+
+        public void FillCbKiesVak()
+        {
+            DataTable dtVakken = new DBS().GetVakken();
+            List<Vakken> lstVakken = new List<Vakken>();
+
+            foreach (DataRow drVakken in dtVakken.Rows)
+            {
+                lstVakken.Add(new Vakken() { VakId = drVakken[0].ToString(), VakNaam = drVakken[1].ToString() });
+            }
+            cbKiesVak.ItemsSource = lstVakken;
+        }
+
+        private void cbKiesVak_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbKiesVak.SelectedItem != null)
+            {
+                sGekozenVakId = ((Vakken)(cbKiesVak.SelectedItem)).VakId;
+                FillCbLesonderdelen();
+            }
+        }
+
+        public void FillCbLesonderdelen()
+        {
+            DataTable dtLesonderwerpen = new DBS().Search("lesonderwerp", "VakID", sGekozenVakId);
+            List<Lesonderdelen> lstLesonderwerpen = new List<Lesonderdelen>();
+            if (dtLesonderwerpen.Rows.Count != 0)
+            {
+                foreach (DataRow drVakken in dtLesonderwerpen.Rows)
+                {
+                    lstLesonderwerpen.Add(new Lesonderdelen() { LesonderdeelId = drVakken[0].ToString(), Lesonderdeel = drVakken[1].ToString() });
+                }
+                cbKiesLesonderdeel.ItemsSource = lstLesonderwerpen;
+                cbKiesLesonderdeel.IsEnabled = true;
+            }
+            else
+            {
+                cbKiesLesonderdeel.IsEnabled = false;
+                cbKiesLesonderdeel.ItemsSource = null;
+                MessageBox.Show("Het gekozen vak heeft nog geen lesonderwerpen, voeg deze allereerst toe.", "Voeg een lesonderwerp toe");
+            }
+        }
+
+        private void lvLeerlingen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lvLeerlingen.SelectedItem != null)
+            {
+                sGekozenLeerlingUsername = ((LvLeerlingInfo)(lvLeerlingen.SelectedItem)).LeerlingUsername;
+            }
+        }
+    }
+}
